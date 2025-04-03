5a384a0c2acd48a8dce29cd9205d07440ebfad3a
almeidalhs <almeidalhs@gmail.com>
2025-04-04 01:53:28 +1300

    change complete status to true

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('todos/', views.TodoListCreate.as_view(), name='todo-list-create'),
-    path('todos/<int:pk>/', views.TodoRetrieveUpdateDestroy.as_view()),
-    path('todos/<int:pk>/complete/', views.TodoToggleComplete.as_view()),
+    path('todos/<int:pk>/', views.TodoRetrieveUpdateDestroy.as_view(), name='todo-update'),
+    path('todos/<int:pk>/complete/', views.TodoToggleComplete.as_view(), name='todo-complete'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
